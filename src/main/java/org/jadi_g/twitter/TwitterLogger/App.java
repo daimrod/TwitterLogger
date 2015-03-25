c6e36d5adc3c02fd73245329e8b6b72603bfdf05
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2015-03-25 15:46:11 +0100

    initial commit

---
+++ src/main/java/org/jadi_g/twitter/TwitterLogger/App.java
@@ -0,0 +1,13 @@
+package org.jadi_g.twitter.TwitterLogger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
